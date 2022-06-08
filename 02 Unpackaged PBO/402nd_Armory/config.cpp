f9458f617cdeec87ee8db8e59bc852870e07b560
thulsonASU <thulson@asu.edu>
2022-06-07 23:06:10 -0700

    Unpackaged PBO due to size limit

---
+++ 02 Unpackaged PBO/402nd_Armory/config.cpp
@@ -0,0 +1,25 @@
+/*
+	402nd Bad Company Armory
+	Azzamean, Vespade & BoomMicGuy
+	Last Updated: 6/6/2022
+	Created On: 01/29/2021
+*/
+
+// START OF FILE -----------------------------------------------------------------------------------------------------
+
+//CfgPatches
+#include "CfgPatches.hpp"
+// CfgAddons
+#include "CfgAddons.hpp"
+// ADDING ITEMS TO EDITOR
+#include "CfgEditorCategories.hpp"
+// DEFINING EDITOR NAME
+#include "CfgEditorSubcategories.hpp"
+// ADDING MURK PELICAN FROM VESPADE PELICAN
+#include "CfgVehicles.hpp"
+// ADDING ODST VISOR IN GLASSES
+#include "cfgGlasses.hpp"
+// START OF CfgWeapons
+#include "CfgWeapons.hpp"
+
+// END OF FILE -----------------------------------------------------------------------------------------------------
